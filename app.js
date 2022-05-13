65af73e1fb4c10a9f839392d836f04e3aafcebf1
MildredAkpeneAkuVittor <82453885+MildredAkpeneAkuVittor@users.noreply.github.com>
2022-05-13 10:53:45 +0000

    test

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
 new Vue({
     el:'#newApp',
     data:{
-        name: 'Mimmie',
-        age: 39,
+        // name: 'Mimmie',
+        // age: 39,
         website: 'www.google.com',
         websiteTag:'<a href="website">google</a>',
         x:0,
-        y:0
-
-
+        y:0,
+        name: '',
+        age: ''
     },
     methods: {
-        greet:function (){
-           return 'Heya' + ' ' + this.name; 
+        // greet:function (){
+        //    return 'Heya' + ' ' + this.name; 
+        // },
+        // add:function (inc){
+        //     this.age += inc
+        // },
+        // subtract:function (dec){
+        //     this.age -= dec
+        // },
+        //  updateXY:function (event){
+        //     this.x = event.offsetX;
+        //     this.y = event.offsetY;
+        // },
+        // linkroute:function () {
+        //   alert('clicked once')  
+        // }
+        nameLog:function () {
+            console.log('hello')
         },
-        add:function (inc){
-            this.age += inc
-        },
-        subtract:function (dec){
-            this.age -= dec
-        },
-         updateXY:function (event){
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-        }
-    }
+        ageLog:function () {
+          console.log('hi')
+        }   
+     }
     }
 );
